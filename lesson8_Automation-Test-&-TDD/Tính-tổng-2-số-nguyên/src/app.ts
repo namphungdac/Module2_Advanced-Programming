c00ad7ab98aa6d9b24cbe5a5d23bceea2eb32fc0
unknown <phungdacnam1997@gmail.com>
2023-04-12 15:18:30 +0700

    1

--- lesson8_Automation-Test-&-TDD/Tính-tổng-2-số-nguyên/src/app.ts
+++ lesson8_Automation-Test-&-TDD/Tính-tổng-2-số-nguyên/src/app.ts
@@ -1,5 +1,12 @@
 export class Calculator {
     public static add(x:number, y: number): number {
+        if (x/2 + y/2 >= Number.MAX_VALUE /2){
+            throw new Error('Error')
+        }
+
+        if ((x/2 + y/2) <= -Number.MAX_VALUE/2){
+            throw new Error('Error')
+        }
         return x + y;
     }
 }
--- lesson8_Automation-Test-&-TDD/Tính-tổng-2-số-nguyên/src/test/app.test.ts
+++ lesson8_Automation-Test-&-TDD/Tính-tổng-2-số-nguyên/src/test/app.test.ts
@@ -4,9 +4,46 @@ describe("add function", () => {
     test("add two number", () => {
         const number1 = 1;
         const number2 = 2;
-
         const result = 3;
 
         expect(Calculator.add(number1, number2)).toBe(result);
+    });
+
+    test("add two number < 0", () => {
+        const number1 = -1;
+        const number2 = -2;
+        const result = -3;
+
+        expect(Calculator.add(number1, number2)).toBe(result);
+    });
+
+    test("add two number with number1 < 0 and number2 > 0", () => {
+        const number1 = -1;
+        const number2 = 2;
+        const result = 1;
+
+        expect(Calculator.add(number1, number2)).toBe(result);
+    });
+
+    test("add two number with number1 max number type", () => {
+        const number1 = Number.MAX_VALUE;
+        const number2 = 1;
+        const result = 'Error';
+        try {
+            return Calculator.add(number1, number2)
+        }catch (e) {
+            expect(e.message).toBe(result);
+        }
+    });
+
+    test("add two number with number1 min number type", () => {
+        const number1 = -Number.MAX_VALUE;
+        const number2 = -1;
+        const result = "Error";
+        try {
+            return Calculator.add(number1, number2)
+        }catch (e) {
+            expect(e.message).toBe(result);
+        }
     });
 });
